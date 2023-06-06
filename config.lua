6e1f5cccaa5056e70db652f8fd0e08535f8dc5fe
Tyler Brooks <doogy107@gmail.com>
2023-06-06 16:32:15 -0400

    change the luxury vehicle shop blip name to luxury vehicle import

--- config.lua
+++ config.lua
@@ -130,7 +130,7 @@ Config.Shops = {
             ['size'] = 2.75 -- size of the vehicles zones
         },
         ['Job'] = 'cardealer', -- Name of job or none
-        ['ShopLabel'] = 'Luxury Vehicle Shop',
+        ['ShopLabel'] = 'Luxury Vehicle Import',
         ['showBlip'] = true, -- true or false
         ['blipSprite'] = 326, -- Blip sprite
         ['blipColor'] = 3, -- Blip color
